2e1ab1739d9e434f1eedf44ee66a29cdefab4efd
icejoywoo <icejoywoo@gmail.com>
2014-01-31 18:59:01 +0800

    update guava predicate demo

--- src/main/java/guava/functional/PredicateDemo.java
+++ src/main/java/guava/functional/PredicateDemo.java
@@ -1,6 +1,10 @@
 package guava.functional;
 
 import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -11,12 +15,13 @@ import com.google.common.base.Predicate;
  */
 public class PredicateDemo {
     public static void main(String[] args) {
-        Predicate p = new Predicate() {
+        boolean result = Iterables.all(Arrays.asList(11, 12), new Predicate<Integer>() {
 
             @Override
-            public boolean apply(java.lang.Object input) {
-                return false;
+            public boolean apply(Integer input) {
+                return input > 10;
             }
-        };
+        });
+        System.out.println(result);
     }
 }

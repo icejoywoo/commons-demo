3a8b7d898cfc20d608b7a0b07919372393113dd5
icejoywoo <icejoywoo@gmail.com>
2014-02-07 16:26:33 +0800

    update

--- src/main/java/guava/strings/JoinerDemo.java
+++ src/main/java/guava/strings/JoinerDemo.java
@@ -1,6 +1,7 @@
 package guava.strings;
 
 import com.google.common.base.Joiner;
+import com.google.common.base.Objects;
 
 import java.util.Arrays;
 
@@ -12,10 +13,27 @@ import java.util.Arrays;
  * Guava Strings Joiner.
  */
 public class JoinerDemo {
+    static class Person {
+        Person(String name) {
+            this.name = name;
+        }
+
+        String name;
+
+        @Override
+        public String toString() {
+            return Objects.toStringHelper(this)
+                    .add("name", this.name)
+                    .toString();
+        }
+    }
     public static void main(String[] args) {
         // joiner instances are always immutable. The joiner configuration methods will always return a new Joiner
         Joiner joiner = Joiner.on(';').skipNulls(); // skip null
         System.out.println(joiner.join("Harry", null, "Ron", "Hermione"));
+        System.out.println(Joiner.on(';').useForNull("no value").join("Harry", null, "Ron", "Hermione"));
+        // calling .toString() to join
+        System.out.println(Joiner.on(';').useForNull("no value").join(new Person("Harry"), null, new Person("Ron"), "Hermione"));
 
         try {
             System.out.println(Joiner.on(',').join("Harry", null, "Ron", "Hermione")); // raise NullPointerException

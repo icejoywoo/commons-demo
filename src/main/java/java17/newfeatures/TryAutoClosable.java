f033cf65e367b2200690af1c491e1d1183f6fbd6
icejoywoo <icejoywoo@icejoywoodeMacBook-Pro.local>
2017-03-01 19:56:01 +0800

    update

--- src/main/java/java17/newfeatures/TryAutoClosable.java
+++ src/main/java/java17/newfeatures/TryAutoClosable.java
@@ -1,8 +1,6 @@
 package java17.newfeatures;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,12 +10,30 @@ import java.io.IOException;
  * jdk1.7 new feature: try auto-closable resource.
  */
 public class TryAutoClosable {
+
+    static class MyFileOutputStream extends FileOutputStream {
+
+        public MyFileOutputStream(String name) throws FileNotFoundException {
+            super(name);
+        }
+
+        public void close() throws IOException {
+            super.close();
+            System.err.println("close method is invoked.");
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         try (BufferedReader br = new BufferedReader(new FileReader("./src/main/resources/test.ftl"))) {
             String line;
             while ((line = br.readLine()) != null) {
                 System.out.println(line);
             }
+        }
+
+        try (OutputStream out = new MyFileOutputStream("/tmp/test")){
+            out.write("そら".getBytes());
+            throw new IOException();
         }
     }
 }
